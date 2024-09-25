61bbe7f2a10e8b4505d6a5cf37afc68d36c992cd
Praveen Kumar <praveen9166050@gmail.com>
2024-09-26 00:03:08 +0530

    Cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.js";
 import CustomError from "./utils/CustomError.js";
@@ -10,6 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
