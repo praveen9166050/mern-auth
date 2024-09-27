a698ec0941b35e3457754cb172d5bfa66962c1c8
Praveen Kumar <praveen9166050@gmail.com>
2024-09-28 00:01:03 +0530

    Cors client url update

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
